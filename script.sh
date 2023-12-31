a507a8761ec7dd3c7cb7b01a6cdf1a4863f1cc78
Roberto Valente <73560955+RobertoValente@users.noreply.github.com>
2023-12-31 02:47:09 +0000

    Added Logs: Locally and Discord Webhooks (Optionally) + Code Organization

--- script.sh
+++ script.sh
@@ -8,12 +8,33 @@ DB_NAME="XXX"
 EXTERNAL_DEVICE_PATH="XXX"
 FOLDERNAME="XXX"
 FILENAME="$(date +'DATABASE_%m-%d-%Y').sql"
+SEND_DISCORDWEBHOOK="NO" # Options: YES or NO
+DISCORD_WEBHOOKURL="XXX"
 ### ======================================= ###
 
-if ! ping -q -c 1 -W 1 google.com > /dev/null; then
-    echo "/!\ > Not connected to WIFI!"
-    exit 1
-fi
+sendMessage() {
+   local message=$1
+
+   if [ "$SEND_DISCORDWEBHOOK" = "YES" ]; then
+      if ! ping -q -c 1 -W 1 google.com > /dev/null; then
+         echo "/!\ > Not connected to WIFI!" >> logsBackup.txt
+         exit
+      else
+         curl -X POST $DISCORD_WEBHOOKURL  -H "Content-Type: application/json" -d "{\"content\": \"\`\`\`css\n${message}\`\`\`\"}"
+      fi
+   fi
+   echo "$message" >> logsBackup.txt
+}
+
+createBackup() {
+   local where=$1
+
+   if [ "$where" = "locally" ]; then
+      mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME > "temp$FOLDERNAME/$FILENAME"
+   else
+      mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME > "$EXTERNAL_DEVICE_PATH/$FOLDERNAME/$FILENAME"
+   fi
+}
 
 if [ -d "$EXTERNAL_DEVICE_PATH" ]; then
    ### External Device Connected!
@@ -22,14 +43,14 @@ if [ -d "$EXTERNAL_DEVICE_PATH" ]; then
    mkdir -p "$EXTERNAL_DEVICE_PATH/$FOLDERNAME"
 
    ### Making the Backup
-   mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME > "$EXTERNAL_DEVICE_PATH/$FOLDERNAME/$FILENAME"
+   createBackup "external"
 
    ### Checking for Errors on Command Execution
    if [ $? -ne 0 ]; then
-      echo "/!\ > Error executing 'mysqldump'!"
+      sendMessage "[$FILENAME] > Error while executing command: mysqldump"
       exit 1
    fi
-   echo "[$FILENAME] > Backup Created on External Device!"
+   sendMessage "[$FILENAME] > Backup Created on External Device!"
 else
    ### External Device NOT Connected!
 
@@ -37,12 +58,12 @@ else
    mkdir -p "temp$FOLDERNAME"
 
    ### Making the Backup
-   mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME > "temp$FOLDERNAME/$FILENAME"
+   createBackup "locally"
 
    ### Checking for Errors on Command Execution
    if [ $? -ne 0 ]; then
-      echo "/!\ > Error executing 'mysqldump'!"
+      sendMessage "[$FILENAME] > Error while executing command: mysqldump"
       exit 1
    fi
-   echo "[$FILENAME] > Backup Created Locally!"
+   sendMessage "[$FILENAME] > Backup Created Locally!"
 fi
